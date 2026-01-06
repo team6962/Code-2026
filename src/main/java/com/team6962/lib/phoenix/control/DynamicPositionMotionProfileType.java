8c0343ecf7dadf83772453835cfd83d5cf48b3b2
Brian <74162951+codeNinja-1@users.noreply.github.com>
2026-01-05 17:45:23 -0800

    Add comments to DynamicPositionMotionProfileType

--- src/main/java/com/team6962/lib/phoenix/control/DynamicPositionMotionProfileType.java
+++ src/main/java/com/team6962/lib/phoenix/control/DynamicPositionMotionProfileType.java
@@ -1,6 +1,22 @@
 package com.team6962.lib.phoenix.control;
 
+/**
+ * Enum representing different shapes of motion profiles that can be used for
+ * position control with dynamic constraints.
+ */
 public enum DynamicPositionMotionProfileType {
+    /**
+     * Trapezoidal motion profile shape, characterized by constant acceleration
+     * to a peak velocity, cruising at that velocity, and then constant
+     * deceleration to the target velocity.
+     */
     Trapezoidal,
+    /**
+     * Exponential motion profile shape, defined by solving the differential
+     * equation:
+     * <pre>
+     * 12 = kv * dx/dt + ka * d²x/dt²
+     * </pre>
+     */
     Exponential
 }
