95fc55211c2c00379a610077420e1926d11dec27
Brian <74162951+codeNinja-1@users.noreply.github.com>
2026-01-05 17:45:14 -0800

    Add comments to ControlOutputType

--- src/main/java/com/team6962/lib/phoenix/control/ControlOutputType.java
+++ src/main/java/com/team6962/lib/phoenix/control/ControlOutputType.java
@@ -1,7 +1,22 @@
 package com.team6962.lib.phoenix.control;
 
+/**
+ * Enum representing different units of output for motor control.
+ */
 public enum ControlOutputType {
+    /**
+     * Output in volts, typically in the range of -12V to +12V. Trapezoidal
+     * commutation is used.
+     */
     Voltage,
+    /**
+     * Output in volts, typically in the range of -12V to +12V. Field oriented
+     * control is used instead of trapezoidal commutation.
+     */
     VoltageFOC,
+    /**
+     * Output in torque current (amps), typically in the range of -120A to
+     * +120A. Field oriented control is used instead of trapezoidal commutation.
+     */
     TorqueCurrentFOC
 }
