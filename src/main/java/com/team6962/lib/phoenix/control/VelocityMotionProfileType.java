97eea88eedca76ac0149847beabc69c5934a463c
Brian <74162951+codeNinja-1@users.noreply.github.com>
2026-01-05 17:45:41 -0800

    Add comments to VelocityMotionProfileType

--- src/main/java/com/team6962/lib/phoenix/control/VelocityMotionProfileType.java
+++ src/main/java/com/team6962/lib/phoenix/control/VelocityMotionProfileType.java
@@ -1,6 +1,19 @@
 package com.team6962.lib.phoenix.control;
 
+/**
+ * Enum representing different shapes of motion profiles that can be used for
+ * velocity control.
+ */
 public enum VelocityMotionProfileType {
+    /**
+     * No motion profiling is applied; only PID, static friction feedforward,
+     * gravity feedforward, and velocity feedforward are used.
+     */
     None,
+    /**
+     * Trapezoidal motion profile shape, characterized by constant acceleration
+     * to a peak velocity, cruising at that velocity, and then constant
+     * deceleration to the target velocity.
+     */
     Trapezoidal
 }
