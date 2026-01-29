60fd12849a12211bc94b9078fcfd49619392630d
styrofoam23 <aidenwows@gmail.com>
2026-01-28 18:38:59 -0800

    added a second motor to follow the first one
    it goes the opposite direction
    did this because thats how build designed it

--- src/main/java/frc/robot/subsystems/shooterrollers/ShooterRoller.java
+++ src/main/java/frc/robot/subsystems/shooterrollers/ShooterRoller.java
@@ -7,10 +7,10 @@ import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.CoastOut;
-import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.fasterxml.jackson.databind.JsonSerializable.Base;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 
 import dev.doglog.DogLog;
 import edu.wpi.first.units.measure.AngularVelocity;
@@ -18,20 +18,22 @@ import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import java.util.Scanner; // Import the Scanner class
-
 //creates status signals
 
 //REMEMBER: The build team for some unexplainable reason will use TWO motors for the shooter roller
 //rewrite code to work for two motors
 //why did the build team decide to do this
 //remember: ask shooter roller guy on build to rethink their design choices
-//
+//update: nevermind it ended up being less work because of the follower class
+//though still everyone told me that it was one motor
+//wouldn't 2 make the math harder?
 
 public class ShooterRoller extends SubsystemBase {
     private TalonFX shooterRollerMotor1;
+    private TalonFX shooterRollerMotor2;
     private StatusSignal<AngularVelocity> angVelocity;
     private StatusSignal<Voltage> voltage;
+    
     
 
 
@@ -39,7 +41,8 @@ public class ShooterRoller extends SubsystemBase {
     //update: it's apparently a feed forward thing to tell motors to get from point a to point b
     //new goal: find a way to delete without causing error to reduce processing power
     public ShooterRoller() {
-        shooterRollerMotor1 = new TalonFX(2,new CANBus("drive train"));
+        shooterRollerMotor1 = new TalonFX(20,new CANBus("drive train"));
+        //note: device id is temporary, find out which device id will be used for shooter rollers
         shooterRollerMotor1.getConfigurator().apply(
             new TalonFXConfiguration()
             .withSlot0(
@@ -53,10 +56,26 @@ public class ShooterRoller extends SubsystemBase {
 
             
         );
-        
+        shooterRollerMotor2 = new TalonFX(21,new CANBus("drive train"));
+        //note: device id is temporary, find out which device id will be used for shooter rollers
+        shooterRollerMotor2.getConfigurator().apply(
+            new TalonFXConfiguration()
+            .withSlot0(
+                new Slot0Configs()
+                    .withKV(0.12))
+            .withCurrentLimits(
+                new CurrentLimitsConfigs()
+                    .withStatorCurrentLimitEnable(false)
+                    .withSupplyCurrentLimitEnable(false)
+
+            )
+
+            
+        );
         // defines the variables we are keeping track of
         angVelocity = shooterRollerMotor1.getVelocity();
         voltage = shooterRollerMotor1.getMotorVoltage();
+        shooterRollerMotor2.setControl(new Follower(shooterRollerMotor1.getDeviceID(), MotorAlignmentValue.Opposed));
         
     }
     //makes the motor go
