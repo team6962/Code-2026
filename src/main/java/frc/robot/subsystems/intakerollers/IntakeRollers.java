cf96627aae378281ed718bba116b86ade9f2e05a
The-REAL-Bol <munsoly@gmail.com>
2026-02-03 19:19:45 -0800

    Fixes

--- src/main/java/frc/robot/subsystems/intakerollers/IntakeRollers.java
+++ src/main/java/frc/robot/subsystems/intakerollers/IntakeRollers.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.intakerollers;
 
 import static edu.wpi.first.units.Units.Volts;
@@ -11,7 +7,6 @@ import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.CoastOut;
-import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.team6962.lib.phoenix.StatusUtil;
@@ -24,11 +19,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeRollers extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  //   private String state;
-  //   private int speed;
   private TalonFX intakeMotor;
-
   private StatusSignal<AngularVelocity> velocitySignal;
   private StatusSignal<Current> statorCurrentSignal;
   private StatusSignal<Current> supplyCurrentSignal;
@@ -37,11 +28,13 @@ public class IntakeRollers extends SubsystemBase {
 
   /** Intializes motor and status signals Class for Intake Rollers */
   public IntakeRollers() {
-    this.intakeMotor = new TalonFX(30, new CANBus("canivore")); // temporary
+    this.intakeMotor = new TalonFX(41, new CANBus("subsystem")); // temporary
     TalonFXConfiguration configuration = new TalonFXConfiguration();
-    configuration.CurrentLimits.StatorCurrentLimitEnable = false;
-    configuration.CurrentLimits.SupplyCurrentLimitEnable = false;
-    configuration.Slot0.kP = 1;
+    configuration.Feedback.SensorToMechanismRatio = 5;
+    configuration.CurrentLimits.StatorCurrentLimitEnable = true;
+    configuration.CurrentLimits.SupplyCurrentLimitEnable = true;
+    configuration.CurrentLimits.StatorCurrentLimit = 120;
+    configuration.CurrentLimits.SupplyCurrentLimit = 60;
     intakeMotor.getConfigurator().apply(configuration);
     this.velocitySignal = intakeMotor.getVelocity();
     this.statorCurrentSignal = intakeMotor.getStatorCurrent();
@@ -53,7 +46,7 @@ public class IntakeRollers extends SubsystemBase {
   }
 
   /** Returns command to make the motor move and stop */
-  public Command move(Voltage voltage) {
+  private Command move(Voltage voltage) {
     return startEnd(
         () -> {
           intakeMotor.setControl(new VoltageOut(voltage));
@@ -79,22 +72,6 @@ public class IntakeRollers extends SubsystemBase {
    */
   public Command outtake() {
     return move(Volts.of(-12));
-  }
-
-  /**
-   * Returns a Command where motor moves at a target velocity
-   *
-   * @param target (double)
-   * @return Command
-   */
-  public Command moveAtVelocity(double target) {
-    return startEnd(
-        () -> {
-          intakeMotor.setControl(new VelocityVoltage(target));
-        },
-        () -> {
-          intakeMotor.setControl(new CoastOut());
-        });
   }
 
   /**
@@ -138,12 +115,12 @@ public class IntakeRollers extends SubsystemBase {
     if (simulation != null) {
       simulation.update();
     }
-    DogLog.log("intakeRollers/velocity", getVelocity());
-    DogLog.log("intakeRollers/statorCurrent", getStatorCurrent());
-    DogLog.log("intakeRollers/SupplyCurrent", getSupplyCurrent());
-    DogLog.log("intakeRollers/appliedVoltage", getAppliedVoltage());
     StatusUtil.check(
         BaseStatusSignal.refreshAll(
             velocitySignal, statorCurrentSignal, supplyCurrentSignal, appliedVoltageSignal));
+    DogLog.log("intakeRollers/velocity", getVelocity());
+    DogLog.log("intakeRollers/statorCurrent", getStatorCurrent());
+    DogLog.log("intakeRollers/supplyCurrent", getSupplyCurrent());
+    DogLog.log("intakeRollers/appliedVoltage", getAppliedVoltage());
   }
 }
