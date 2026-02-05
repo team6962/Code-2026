3ae8f25893c6c998ad9956818076c4847c855c17
The-REAL-Bol <munsoly@gmail.com>
2026-02-04 18:24:46 -0800

    updated moment of inertia

--- src/main/java/frc/robot/subsystems/intakerollers/IntakeRollersConstants.java
+++ src/main/java/frc/robot/subsystems/intakerollers/IntakeRollersConstants.java
@@ -6,7 +6,7 @@ import edu.wpi.first.units.measure.Current;
 
 public class IntakeRollersConstants {
     public static final double gearRatio = 5.0;
-    public static final double momentOfInertia = 0.001;
+    public static final double momentOfInertia = 0.00074271944;
     public static final int deviceId = 41;
     public static final Current statorCurrentLimit = Amps.of(120);
     public static final Current supplyCurrentLimit = Amps.of(60);
