8643ca798f51d19b4ce7def4d5234120faa0c81e
styrofoam23 <aidenwows@gmail.com>
2026-02-05 19:38:53 -0800

    removed more unnecessary comments
    removed more unnecessary comments

--- src/main/java/frc/robot/subsystems/shooterrollers/ShooterRoller.java
+++ src/main/java/frc/robot/subsystems/shooterrollers/ShooterRoller.java
@@ -17,7 +17,6 @@ import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-// creates status signals
 /** this is the subsystem for the flywheels that both makes the motor go and records motor values */
 public class ShooterRoller extends SubsystemBase {
   private TalonFX shooterRollerMotor1;
