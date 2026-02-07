97f8df2e9cd62fc588065a8ec6c4839ec802ea71
Brian <74162951+codeNinja-1@users.noreply.github.com>
2026-02-06 17:13:43 -0800

    Add Preferences option for reorienting controls in simulation

--- src/main/java/frc/robot/Preferences.java
+++ src/main/java/frc/robot/Preferences.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import com.team6962.lib.swerve.config.DrivetrainConstants;
+import com.team6962.lib.swerve.config.XBoxTeleopSwerveConstants;
 
 public class Preferences {
   /**
@@ -14,6 +15,14 @@ public class Preferences {
   public static final boolean enablePoseEstimationInSimulation = false;
 
   /**
+   * Whether to reorient the controls in simulation so that pressing the up key moves the robot
+   * up the screen in the Sim GUI. This is recommended to be true when testing using the Sim GUI,
+   * but should usually be false when visualizing with AdvantageScope's 3D field from a driver's
+   * perspective.
+   */
+  public static final boolean reorientControlsInSimulation = true;
+
+  /**
    * Applies the preferences to the given drivetrain constants.
    *
    * @param constants The drivetrain constants to apply the preferences to.
@@ -21,6 +30,17 @@ public class Preferences {
    */
   public static DrivetrainConstants apply(DrivetrainConstants constants) {
     constants.Simulation.EnablePoseEstimation = enablePoseEstimationInSimulation;
+    return constants;
+  }
+
+  /**
+   * Applies the preferences to the given teleop controls constants.
+   * 
+   * @param constants The teleop controls constants to apply the preferences to.
+   * @return The updated teleop controls constants.
+   */
+  public static XBoxTeleopSwerveConstants apply(XBoxTeleopSwerveConstants constants) {
+    constants.ReorientControlsInSimulation = reorientControlsInSimulation;
     return constants;
   }
 }
