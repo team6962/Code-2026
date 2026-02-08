4b4ddb229d8dd070481c8eb2667d6ca6d28e8df6
Brian <74162951+codeNinja-1@users.noreply.github.com>
2026-02-07 17:17:25 -0800

    Tune shooter rollers for shooter A (#44)
    * Tune shooter rollers for shooter A
    
    * Apply spotless

--- src/main/java/frc/robot/subsystems/shooterrollers/ShooterRollersConstants.java
+++ src/main/java/frc/robot/subsystems/shooterrollers/ShooterRollersConstants.java
@@ -4,8 +4,10 @@ import static edu.wpi.first.units.Units.KilogramSquareMeters;
 
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.MomentOfInertia;
 
@@ -16,16 +18,17 @@ public final class ShooterRollersConstants {
   public static final int MOTOR_CAN_ID_1 = 21;
   public static final int MOTOR_CAN_ID_2 = 22;
   public static final DCMotor MOTOR_PHYSICS = DCMotor.getKrakenX60Foc(2);
-  public static final String CANBUS_NAME = "subsystem";
+  public static final String CANBUS_NAME = "subsystems";
   public static final MomentOfInertia MOMENT_OF_INERTIA = KilogramSquareMeters.of(0.003072714);
   public static final TalonFXConfiguration MOTOR_CONFIGURATION =
       new TalonFXConfiguration()
-          .withFeedback(new FeedbackConfigs().withSensorToMechanismRatio(4 / 3))
-          .withSlot0(new Slot0Configs().withKV(0.124137931).withKP(0.01))
+          .withFeedback(new FeedbackConfigs())
+          .withSlot0(new Slot0Configs().withKV(0.124137931).withKS(0.423).withKP(0.1))
           .withCurrentLimits(
               new CurrentLimitsConfigs()
                   .withStatorCurrentLimitEnable(true)
                   .withStatorCurrentLimit(120)
                   .withSupplyCurrentLimitEnable(true)
-                  .withSupplyCurrentLimit(80));
+                  .withSupplyCurrentLimit(80))
+          .withMotorOutput(new MotorOutputConfigs().withInverted(InvertedValue.Clockwise_Positive));
 }
