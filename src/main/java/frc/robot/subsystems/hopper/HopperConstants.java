86fd622dedd64ade5dfb48c4a2e15e8a5fc59d61
The-REAL-Bol <munsoly@gmail.com>
2026-02-10 19:51:25 -0800

    added constants, finished kicker
    variables need to be changed and tuned

--- src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.hopper;
 
+import static edu.wpi.first.units.Units.Amps;
+
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
@@ -9,6 +11,18 @@ public class HopperConstants {
     //kicker
     public static final int KICKER_DEVICE_ID = 31;
     public static final double KICKER_MOMENT_OF_INERTIA = 0.005;
+    public static final double KICKER_GEAR_RATIO = 2.5;
+    public static final TalonFXConfiguration KICKER_MOTOR_CONFIGURATION = 
+      new TalonFXConfiguration()
+          .withFeedback(new FeedbackConfigs().withSensorToMechanismRatio(KICKER_GEAR_RATIO))
+          .withSlot0(new Slot0Configs().withKP(0.02)) // needs to be tuned
+          .withCurrentLimits(
+              new CurrentLimitsConfigs()
+                  .withStatorCurrentLimit(Amps.of(120)) //everything needs to be tuned
+                  .withSupplyCurrentLimit(Amps.of(60)) // current limits might not be neccesary
+                  .withSupplyCurrentLimitEnable(true)
+                  .withStatorCurrentLimitEnable(true));
+
 
     //belt floor
     public static final int BELT_FLOOR_MOTOR_CAN_ID = 30;
--- src/main/java/frc/robot/subsystems/hopper/kicker/Kicker.java
+++ src/main/java/frc/robot/subsystems/hopper/kicker/Kicker.java
@@ -1,22 +1,112 @@
 package frc.robot.subsystems.hopper.kicker;
 
-import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.hardware.TalonFX;
+import static edu.wpi.first.units.Units.Volts;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.CoastOut;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.team6962.lib.phoenix.StatusUtil;
+
+import dev.doglog.DogLog;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.hopper.HopperConstants;
 
 public class Kicker extends SubsystemBase {
     private TalonFX kickerMotor;
-    private StatusSignal AppliedVoltagesignal;
-    private StatusSignal StatorCurrentSignal;
-    private StatusSignal SupplyCurrent; 
-    private StatusSignal MotorVelocity;
+    private StatusSignal<Voltage> appliedVoltageSignal;
+    private StatusSignal<Current> statorCurrentSignal;
+    private StatusSignal<Current> supplyCurrentSignal; 
+    private StatusSignal<AngularVelocity> motorVelocitySignal;
 
     public Kicker() {
+
         this.kickerMotor = new TalonFX(HopperConstants.KICKER_DEVICE_ID);
-  
-        
+        kickerMotor.getConfigurator().apply(HopperConstants.KICKER_MOTOR_CONFIGURATION);
+        this.appliedVoltageSignal = kickerMotor.getMotorVoltage();
+        this.statorCurrentSignal = kickerMotor.getStatorCurrent();
+        this.supplyCurrentSignal = kickerMotor.getSupplyCurrent();
+        this.motorVelocitySignal = kickerMotor.getVelocity();
     }
     
+    /** Returns command to make the motor move and stop */
+    private Command move(Voltage voltage) {
+        return startEnd(
+            () -> {
+            kickerMotor.setControl(new VoltageOut(voltage));
+            },
+            () -> {
+            kickerMotor.setControl(new CoastOut());
+            });
+    }
+    /**
+     * feeds fuel to the shooter 
+     * return
+     */
+    public Command feed() {
+        return move(Volts.of(12));//negative or positive to be tuned
+    }
+    /**
+     * passes fuel back to intake
+     * @return command
+     */
+    public Command reverse() {
+        return move(Volts.of(-12));// negative or positive to be tuned
+    }
+    /**
+     * Takes status signal velocity and returns it as an AngularVelocity
+     *
+     * @return AngularVelocity
+     */
+    public AngularVelocity getVelocity() {
+        return motorVelocitySignal.getValue();
+    }
+
+    /**
+     * Takes status signal Stator Current and returns it as a Current
+     *
+     * @return Current
+     */
+    public Current getStatorCurrent() {
+        return statorCurrentSignal.getValue();
+    }
+
+    /**
+     * Takes status signal Supply Current and returns it as a Current
+     *
+     * @return Current
+     */
+    public Current getSupplyCurrent() {
+        return supplyCurrentSignal.getValue();
+    }
+
+    /**
+     * Takes status signal Applied Voltage and returns it as a Voltage
+     *
+     * @return Voltage
+     */
+    public Voltage getAppliedVoltage() {
+        return appliedVoltageSignal.getValue();
+    }
+
+
+    @Override
+    public void periodic() {
+        // if (simulation != null) {
+        //   simulation.update();
+        // }
+        StatusUtil.check(
+        BaseStatusSignal.refreshAll(
+            motorVelocitySignal, statorCurrentSignal, supplyCurrentSignal, appliedVoltageSignal));
+        DogLog.log("kicker/angularVelocity", getVelocity());
+        DogLog.log("kicker/statorCurrent", getStatorCurrent());
+        DogLog.log("kicker/supplyCurrent", getSupplyCurrent());
+        DogLog.log("kicker/appliedVoltage", getAppliedVoltage());
+  }
 }
+
