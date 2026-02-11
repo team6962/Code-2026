cecf03833d8d666cba7c8e885233796a6ae74e1e
neelabhb-sudo <neelabhb@khanlabschool.org>
2026-02-10 18:52:39 -0800

    Brought over Hopper Sensors code from Hopper Senses branch

---
+++ src/main/java/frc/robot/subsystems/HopperSensors.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.CANdi;
+import com.ctre.phoenix6.hardware.CANrange;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.fasterxml.jackson.databind.introspect.TypeResolutionContext.Empty;
+
+import edu.wpi.first.units.measure.Angle;
+
+public class HopperSensors {
+    private TalonFX motor;
+
+    private CANrange candi;
+    
+    // private StatusSignal<> positionSignal;
+}
