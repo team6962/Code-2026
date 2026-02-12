666ce6eb726a3ca31484e4506e885dde4c581f5c
neelabhb-sudo <neelabhb@khanlabschool.org>
2026-02-11 19:19:46 -0800

    Added Hopper Sensor Constants and establish the different Dog log variables
    Added CanBus Name, Can IDs for Kicker, Upper Hopper, and Lower Hopper, added the different configurations for each sensor. In Hopper Sensors, added the main dogLog variables and added the configuration to the Hopper sensors.

--- src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
@@ -4,7 +4,15 @@ import static edu.wpi.first.units.Units.Amps;
 
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
+import static edu.wpi.first.units.Units.Degrees;
+
+import com.ctre.phoenix6.configs.CANrangeConfiguration;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.FovParamsConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.CANrange;
 
 public class HopperConstants {
   public static final String CANBUS_NAME = "subsystems";
@@ -21,4 +29,34 @@ public class HopperConstants {
                   .withSupplyCurrentLimit(Amps.of(60)) // current limits might not be neccesary
                   .withSupplyCurrentLimitEnable(true)
                   .withStatorCurrentLimitEnable(true));
+    
+    //Sensors
+    public static final String SENSORS_CANBUS_NAME = "subsystem";
+    public static final int KICKER_SENSOR_CAN_ID = 32;
+    public static final int UPPER_HOPPER_CAN_ID = 33;
+    public static final int LOWER_HOPPER_CAN_ID = 34;
+    
+    
+    public static final CANrangeConfiguration KICKER_SENSOR_CONFIGURATION = 
+        new CANrangeConfiguration()
+            .withFovParams(
+                new FovParamsConfigs()
+                    .withFOVRangeX(Degrees.of(6.75))
+                    .withFOVRangeY(Degrees.of(6.75))
+                );
+    
+    public static final CANrangeConfiguration UPPER_HOPPER_CONFIGURATION = 
+        new CANrangeConfiguration()
+            .withFovParams(
+                new FovParamsConfigs()
+                    .withFOVRangeX(Degrees.of(27))
+                    .withFOVRangeY(Degrees.of(6.75))
+                );
+    public static final CANrangeConfiguration LOWER_HOPPER_CONFIGURATION = 
+        new CANrangeConfiguration()
+            .withFovParams(
+                new FovParamsConfigs()
+                    .withFOVRangeX(Degrees.of(6.75))
+                    .withFOVRangeY(Degrees.of(6.75))
+                );
 }
--- src/main/java/frc/robot/subsystems/hopper/sensors/HopperSensors.java
+++ src/main/java/frc/robot/subsystems/hopper/sensors/HopperSensors.java
@@ -1,17 +1,41 @@
 package frc.robot.subsystems.hopper.sensors;
 
+import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.CANdi;
 import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.fasterxml.jackson.databind.introspect.TypeResolutionContext.Empty;
-
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.hopper.HopperConstants;
+import frc.robot.subsystems.intakerollers.IntakeRollersConstants;
 
-public class HopperSensors {
-    private TalonFX motor;
+public class HopperSensors extends SubsystemBase {
 
-    private CANrange candi;
+    private CANrange kickerSensor;
+    private CANrange upperHopperSensor; 
+    private CANrange lowerHopperSensor;
     
-    // private StatusSignal<> positionSignal;
+    private StatusSignal<Distance> kickerDistance;
+    private StatusSignal<Distance> hopperDistance;
+    private StatusSignal<Boolean> hopperFull;
+    private StatusSignal<Boolean> hopperEmpty;
+    private StatusSignal<Boolean> kickerFull;
+    private StatusSignal<Boolean> kickerEmpty;
+
+    public HopperSensors() {
+        kickerSensor =
+            new CANrange(HopperConstants.KICKER_SENSOR_CAN_ID, new CANBus(HopperConstants.SENSORS_CANBUS_NAME));
+        kickerSensor.getConfigurator().apply(HopperConstants.KICKER_SENSOR_CONFIGURATION);
+
+        upperHopperSensor =
+            new CANrange(HopperConstants.UPPER_HOPPER_CAN_ID, new CANBus(HopperConstants.SENSORS_CANBUS_NAME));
+        upperHopperSensor.getConfigurator().apply(HopperConstants.UPPER_HOPPER_CONFIGURATION);
+        
+        lowerHopperSensor =
+            new CANrange(HopperConstants.LOWER_HOPPER_CAN_ID, new CANBus(HopperConstants.SENSORS_CANBUS_NAME));
+        lowerHopperSensor.getConfigurator().apply(HopperConstants.LOWER_HOPPER_CONFIGURATION);
+    }
 }
