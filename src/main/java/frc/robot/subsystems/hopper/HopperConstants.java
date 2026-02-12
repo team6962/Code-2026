d831951a91f69f360139fa1dcb305be4d66dfd76
neelabhb-sudo <neelabhb@khanlabschool.org>
2026-02-11 19:19:46 -0800

    Added Hopper Sensor Constants and establish the different Dog log variables
    Added CanBus Name, Can IDs for Kicker, Upper Hopper, and Lower Hopper, added the different configurations for each sensor. In Hopper Sensors, added the main dogLog variables and added the configuration to the Hopper sensors.

--- src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
@@ -1,9 +1,14 @@
 package frc.robot.subsystems.hopper;
 
+import static edu.wpi.first.units.Units.Degrees;
+
+import com.ctre.phoenix6.configs.CANrangeConfiguration;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.FovParamsConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.CANrange;
 
 public class HopperConstants {
     //kicker
@@ -24,4 +29,33 @@ public class HopperConstants {
                   .withSupplyCurrentLimitEnable(true)
                   .withSupplyCurrentLimit(80));
     
+    //Sensors
+    public static final String SENSORS_CANBUS_NAME = "subsystem";
+    public static final int KICKER_SENSOR_CAN_ID = 32;
+    public static final int UPPER_HOPPER_CAN_ID = 33;
+    public static final int LOWER_HOPPER_CAN_ID = 34;
+    
+    
+    public static final CANrangeConfiguration KICKER_SENSOR_CONFIGURATION = 
+        new CANrangeConfiguration()
+            .withFovParams(
+                new FovParamsConfigs()
+                    .withFOVRangeX(Degrees.of(6.75))
+                    .withFOVRangeY(Degrees.of(6.75))
+                );
+    
+    public static final CANrangeConfiguration UPPER_HOPPER_CONFIGURATION = 
+        new CANrangeConfiguration()
+            .withFovParams(
+                new FovParamsConfigs()
+                    .withFOVRangeX(Degrees.of(27))
+                    .withFOVRangeY(Degrees.of(6.75))
+                );
+    public static final CANrangeConfiguration LOWER_HOPPER_CONFIGURATION = 
+        new CANrangeConfiguration()
+            .withFovParams(
+                new FovParamsConfigs()
+                    .withFOVRangeX(Degrees.of(6.75))
+                    .withFOVRangeY(Degrees.of(6.75))
+                );
 }
