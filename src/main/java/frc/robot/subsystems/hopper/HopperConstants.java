a9c456fd6d57b61fbb0131b39a9ef029c2e897c1
Brian <74162951+codeNinja-1@users.noreply.github.com>
2026-02-18 12:58:05 -0800

    Add hopper sensors (#59)
    * Brought over Hopper Sensors code from Hopper Senses branch
    
    * Move HopperSensors to subsystems/hopper/sensors
    
    * Added Hopper Sensor Constants and establish the different Dog log variables
    
    Added CanBus Name, Can IDs for Kicker, Upper Hopper, and Lower Hopper, added the different configurations for each sensor. In Hopper Sensors, added the main dogLog variables and added the configuration to the Hopper sensors.
    
    * Finished Hopper Sensor Methods, added Dog Logs for everything specified, and added Distance Threshold(needs to be changed)
    
    * Use shared hopper CAN bus name instead of SENSORS_CANBUS_NAME
    
    * Apply spotless
    
    * Added Java docs to Constants and Sensors, Fixed some stuff in sensors
    
    Brian if you see this, I cant change the Can Ids because their is other Can Ids that use it.
    Other changes include adding kicker sensor empty and full and re assigning it to the proper method, refreshing upperHopperDistance and lowerHopperDistance, and changing the methods to be more optimized.
    
    * style: Apply Spotless fixes
    
    * Minor improvements to hopper sensors
    
    Add comments, make distance methods private, improve full/empty logic, change logging path, and correct CAN IDs
    
    ---------
    
    Co-authored-by: neelabhb-sudo <neelabhb@khanlabschool.org>

--- src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
@@ -1,10 +1,15 @@
 package frc.robot.subsystems.hopper;
 
 import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Inches;
 
+import com.ctre.phoenix6.configs.CANrangeConfiguration;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.FovParamsConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import edu.wpi.first.units.measure.Distance;
 
 public class HopperConstants {
   public static final String CANBUS_NAME = "subsystems";
@@ -21,4 +26,44 @@ public class HopperConstants {
                   .withSupplyCurrentLimit(Amps.of(60)) // current limits might not be neccesary
                   .withSupplyCurrentLimitEnable(true)
                   .withStatorCurrentLimitEnable(true));
+
+  // Sensors
+  // The CAN IDs for the sensors: kicker, upper hopper, and lower hopper.
+  public static final int KICKER_SENSOR_CAN_ID = 30;
+  public static final int UPPER_HOPPER_CAN_ID = 32;
+  public static final int LOWER_HOPPER_CAN_ID = 31;
+
+  // Threshold distances for determining if the Kicker is full.
+  public static final Distance KICKER_SENSOR_FULL_THRESHOLD =
+      Inches.of(1.0); // This isn't the correct distance
+
+  // Threshold distances for determining if the Kicker is empty
+  public static final Distance KICKER_SENSOR_EMPTY_THRESHOLD =
+      Inches.of(2.0); // This isn't the correct distance
+
+  // Threshold distances for determining if the Hopper is full
+  public static final Distance UPPER_HOPPER_SENSOR_FULL_THRESHOLD =
+      Inches.of(1.0); // This isn't the correct distance
+
+  // Threshold distances for determining if the Hopper is empty
+  public static final Distance LOWER_HOPPER_SENSOR_EMPTY_THRESHOLD =
+      Inches.of(1.0); // This isn't the correct distance
+
+  public static final CANrangeConfiguration KICKER_SENSOR_CONFIGURATION =
+      new CANrangeConfiguration()
+          .withFovParams(
+              new FovParamsConfigs()
+                  .withFOVRangeX(Degrees.of(6.75))
+                  .withFOVRangeY(Degrees.of(6.75)));
+
+  public static final CANrangeConfiguration UPPER_HOPPER_CONFIGURATION =
+      new CANrangeConfiguration()
+          .withFovParams(
+              new FovParamsConfigs().withFOVRangeX(Degrees.of(27)).withFOVRangeY(Degrees.of(6.75)));
+  public static final CANrangeConfiguration LOWER_HOPPER_CONFIGURATION =
+      new CANrangeConfiguration()
+          .withFovParams(
+              new FovParamsConfigs()
+                  .withFOVRangeX(Degrees.of(6.75))
+                  .withFOVRangeY(Degrees.of(6.75)));
 }
