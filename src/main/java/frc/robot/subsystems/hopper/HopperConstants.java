51f0b828223e1efc75205af97032a0a05987272d
styrofoam23 <aidenwows@gmail.com>
2026-02-18 16:12:33 -0800

    Belt floor (#53)
    * Added hopper constants and added kicker code
    
    * Brought over Hopper Sensors code from Hopper Senses branch
    
    * added my files
    
    added my files
    
    * Organized all hopper code
    
    * fixed package statement
    
    * added code to make command to move belt floor, log variables, create constants, etc
    
    basically a simpler version of shooter roller for now, but that should be okay for this application.
    
    * changed code to input voltage instead of velocity
    
    Did this because Brian told me to. Tested in elastic and reveals expected results.
    
    * updated a bunch of constants
    
    updated a bunch of constants according to tech binder and brian's advice. also deleted some imports that were making the code yellow.
    
    * added a bunch of java doc comments
    
    added a bunch of java doc comments so that I don't need to add them later
    
    * Ran spotless
    
    ran spotless to clean up code
    
    * renamed a bunch of stuff
    
    renamed a bunch of stuff according to what it says it should be on the github issues tab
    
    * ran spotless again
    
    ran spotless again
    
    * prepared branch for merge
    
    prepared branch for merge
    
    * style: Apply Spotless fixes
    
    * renamed beltFloor file to beltfloor
    
    renamed beltFloor file to beltfloor
    
    * fixed names of field names
    
    first letters are now lower case
    
    * fixed a javadoc comment not working
    
    fixed a javadoc comment not working
    
    * optimized linear velocity calculation
    
    replaced the RotationsPerSecond with RadiansPerSecond and removed the 2 pi multiplier
    
    * converted BELT_FLOOR_PULLEY_RADIUS to Distance
    
    also edited linear velocity function to account for this
    
    * Deleted some stuff in robot container
    
    did this so that people can create a new hopper class later.
    
    * ran spotless
    
    ran spotless
    
    * Fix folder capitalization
    
    * Delete src/main/java/frc/robot/subsystems/hopper/beltFloor directory
    
    * Added Javadocs, changed feedDump from public to private, and updated the data type from double to Voltage.
    
    I also had to add this: "Voltage target = edu.wpi.first.units.Units.Volts.of(newVoltageDouble);", but I am not sure if its good for this format.
    
    * style: Apply Spotless fixes
    
    * Apply spotless
    
    * Add some more Javadoc comments
    
    * Remove extra comment
    
    ---------
    
    Co-authored-by: The-REAL-Bol <munsoly@gmail.com>
    Co-authored-by: neelabhb-sudo <neelabhb@khanlabschool.org>
    Co-authored-by: styrofoam23 <173725734+styrofoam23@users.noreply.github.com>
    Co-authored-by: Brian <74162951+codeNinja-1@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
@@ -3,13 +3,17 @@ package frc.robot.subsystems.hopper;
 import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.KilogramSquareMeters;
 
 import com.ctre.phoenix6.configs.CANrangeConfiguration;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.FovParamsConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.MomentOfInertia;
 
 public class HopperConstants {
   public static final String CANBUS_NAME = "subsystems";
@@ -27,6 +31,22 @@ public class HopperConstants {
                   .withSupplyCurrentLimitEnable(true)
                   .withStatorCurrentLimitEnable(true));
 
+  // belt floor
+  public static final int BELT_FLOOR_MOTOR_CAN_ID = 30;
+  public static final DCMotor BELT_FLOOR_MOTOR_PHYSICS = DCMotor.getKrakenX60Foc(1);
+  public static final double BELT_FLOOR_GEARING = 2.5;
+  public static final MomentOfInertia BELT_FLOOR_MOMENT_OF_INERTIA = KilogramSquareMeters.of(0.02);
+  public static final Distance BELT_FLOOR_PULLEY_RADIUS = Inches.of(0.51);
+  public static final TalonFXConfiguration BELT_FLOOR_MOTOR_CONFIG =
+      new TalonFXConfiguration()
+          .withFeedback(new FeedbackConfigs().withSensorToMechanismRatio(BELT_FLOOR_GEARING))
+          .withSlot0(new Slot0Configs().withKV(0.124137931).withKP(0.01)) // temp change later
+          .withCurrentLimits(
+              new CurrentLimitsConfigs()
+                  .withStatorCurrentLimitEnable(true)
+                  .withStatorCurrentLimit(80)
+                  .withSupplyCurrentLimitEnable(true)
+                  .withSupplyCurrentLimit(40));
   // Sensors
   // The CAN IDs for the sensors: kicker, upper hopper, and lower hopper.
   public static final int KICKER_SENSOR_CAN_ID = 30;
