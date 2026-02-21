38463f5d10cfbcd49d62f245cbc97f57611c199b
Brian <74162951+codeNinja-1@users.noreply.github.com>
2026-02-21 10:18:53 -0800

    Implement fuel intaking simulation

--- src/main/java/frc/robot/subsystems/visualizer/RobotVisualizationConstants.java
+++ src/main/java/frc/robot/subsystems/visualizer/RobotVisualizationConstants.java
@@ -1,9 +1,11 @@
 package frc.robot.subsystems.visualizer;
 
 import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Inches;
 
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
 
 /** Constants used for visualizing the robot in AdvantageScope. */
 public class RobotVisualizationConstants {
@@ -16,4 +18,12 @@ public class RobotVisualizationConstants {
 
   /** The angle of the intake relative to the horizontal plane. */
   public static Angle intakeAngle = Degrees.of(-18);
+
+  // Hopper dimensions for visualization and simulation
+  public static int hopperExtendedLength = 4;
+  public static int hopperRetractedLength = 2;
+  public static int hopperWidth = 4;
+  public static int maxExtendedFuel = 48;
+  public static int maxRetractedFuel = 24;
+  public static Distance fuelSpacing = Inches.of(6);
 }
