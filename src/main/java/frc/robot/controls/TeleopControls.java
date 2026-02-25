d8e455d34116730f9fd6b48b9e3f9e53af418bac
allenx-afk <allenx@khanlabschool.org>
2026-02-24 18:35:21 -0800

    Commented out teleop controls that won't be used at week 0

--- src/main/java/frc/robot/controls/TeleopControls.java
+++ src/main/java/frc/robot/controls/TeleopControls.java
@@ -1,12 +1,12 @@
 package frc.robot.controls;
 
-import static edu.wpi.first.units.Units.Radians;
+// import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
-import com.team6962.lib.swerve.commands.TeleopSwerveCommand;
+// import com.team6962.lib.swerve.commands.TeleopSwerveCommand;
 import com.team6962.lib.swerve.commands.XBoxTeleopSwerveCommand;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
+// import edu.wpi.first.math.geometry.Pose2d;
+// import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.RobotState;
@@ -16,17 +16,17 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.RobotContainer;
-import frc.robot.auto.AutoClimb;
-import frc.robot.auto.DriveToClump;
-import frc.robot.subsystems.climb.ClimbConstants;
-import frc.robot.subsystems.hood.ShooterHoodConstants;
+// import frc.robot.auto.AutoClimb;
+// import frc.robot.auto.DriveToClump;
+// import frc.robot.subsystems.climb.ClimbConstants;
+// import frc.robot.subsystems.hood.ShooterHoodConstants;
 import frc.robot.subsystems.intakeextension.IntakeExtensionConstants;
-import frc.robot.subsystems.turret.TurretConstants;
+// import frc.robot.subsystems.turret.TurretConstants;
 
 public class TeleopControls {
   private RobotContainer robot;
-  private AutoClimb autoClimb;
-  private DriveToClump driveToClump;
+//   private AutoClimb autoClimb;
+//   private DriveToClump driveToClump;
   private CommandXboxController driver = new CommandXboxController(0);
   private CommandXboxController operator = new CommandXboxController(1);
 
@@ -34,8 +34,8 @@ public class TeleopControls {
 
   public TeleopControls(RobotContainer robot) {
     this.robot = robot;
-    this.autoClimb = new AutoClimb(robot);
-    this.driveToClump = new DriveToClump(robot);
+    // this.autoClimb = new AutoClimb(robot);
+    // this.driveToClump = new DriveToClump(robot);
   }
 
   public void configureBindings() {
@@ -62,44 +62,44 @@ public class TeleopControls {
     // Driver right trigger is boost (configured by XBoxTeleopSwerveCommand)
     // Driver left trigger is super boost (configured by XBoxTeleopSwerveCommand)
 
-    // Auto Climb and Unclimb
-    driver.b().onTrue(autoClimb.climb());
-    driver.x().onTrue(autoClimb.unclimb());
+    // // Auto Climb and Unclimb
+    // driver.b().onTrue(autoClimb.climb());
+    // driver.x().onTrue(autoClimb.unclimb());
 
-    // Auto Depot
-    driver
-        .leftBumper()
-        .whileTrue(
-            Commands.sequence(
-                this.robot
-                    .getSwerveDrive()
-                    .driveTo(
-                        new Pose2d(
-                            1.518,
-                            5.947,
-                            new Rotation2d(
-                                Radians.of(
-                                    Math.PI)))), // rough position estimate based on simulation, not
-                // exact
-                this.robot.getIntakeExtension().extend(),
-                Commands.parallel(
-                    this.robot
-                        .getSwerveDrive()
-                        .driveTo(
-                            new Pose2d(
-                                0.546,
-                                5.947,
-                                new Rotation2d(Radians.of(Math.PI)))), // also rough estimate
-                    this.robot.getIntakeRollers().intake())));
+    // // Auto Depot
+    // driver
+    //     .leftBumper()
+    //     .whileTrue(
+    //         Commands.sequence(
+    //             this.robot
+    //                 .getSwerveDrive()
+    //                 .driveTo(
+    //                     new Pose2d(
+    //                         1.518,
+    //                         5.947,
+    //                         new Rotation2d(
+    //                             Radians.of(
+    //                                 Math.PI)))), // rough position estimate based on simulation, not
+    //             // exact
+    //             this.robot.getIntakeExtension().extend(),
+    //             Commands.parallel(
+    //                 this.robot
+    //                     .getSwerveDrive()
+    //                     .driveTo(
+    //                         new Pose2d(
+    //                             0.546,
+    //                             5.947,
+    //                             new Rotation2d(Radians.of(Math.PI)))), // also rough estimate
+    //                 this.robot.getIntakeRollers().intake())));
 
-    driver // Auto Drive to Outpost
-        .rightBumper()
-        .whileTrue(
-            this.robot
-                .getSwerveDrive()
-                .driveTo(
-                    new Pose2d(
-                        0.6, 0.65, new Rotation2d(Radians.of(Math.PI))))); // also a rough estimate
+    // driver // Auto Drive to Outpost
+    //     .rightBumper()
+    //     .whileTrue(
+    //         this.robot
+    //             .getSwerveDrive()
+    //             .driveTo(
+    //                 new Pose2d(
+    //                     0.6, 0.65, new Rotation2d(Radians.of(Math.PI))))); // also a rough estimate
 
     // Dump fuel
     driver
@@ -109,23 +109,23 @@ public class TeleopControls {
                 this.robot.getIntakeRollers().outtake(), this.robot.getHopper().dump()));
 
     // Intake and drive to fuel clump
-    driver.rightStick().whileTrue(driveToClump.driveToClump());
+    // driver.rightStick().whileTrue(driveToClump.driveToClump());
 
     // Intake without driving
     driver
         .start()
         .whileTrue(this.robot.getIntakeRollers().intake()); // this might be switched with back
 
-    // Manual climb controls
-    operator.a().onTrue(robot.getClimb().descend()); // Lower climb
-    operator.b().onTrue(robot.getClimb().pullUp()); // Lift robot
-    operator.y().onTrue(robot.getClimb().elevate()); // Raise climb
+    // // Manual climb controls
+    // operator.a().onTrue(robot.getClimb().descend()); // Lower climb
+    // operator.b().onTrue(robot.getClimb().pullUp()); // Lift robot
+    // operator.y().onTrue(robot.getClimb().elevate()); // Raise climb
 
     // Unjam hopper
     operator.leftBumper().whileTrue(robot.getHopper().unjam());
 
     // Disable shooting
-    operator.leftTrigger().whileTrue(robot.getShooterRollers().shoot(RotationsPerSecond.of(0)));
+    // operator.leftTrigger().whileTrue(robot.getShooterRollers().shoot(RotationsPerSecond.of(0)));
 
     // Toggle fine control mode
     operator
@@ -136,40 +136,40 @@ public class TeleopControls {
                   fineControl = !fineControl;
                 }));
 
-    // Force shooting
-    operator.rightTrigger().whileTrue(Commands.print("Force Shoot"));
+    // Shoot
+    operator.rightTrigger().whileTrue(robot.getShooterRollers().shoot(RotationsPerSecond.of(1))); //Incorrect number
 
-    // Pass fuel to alliance zone
-    operator.back().whileTrue(Commands.print("Pass Left")); // this might be switched with start
-    operator.start().whileTrue(Commands.print("Pass Right")); // this might be switched with back
+    // // Pass fuel to alliance zone
+    // operator.back().whileTrue(Commands.print("Pass Left")); // this might be switched with start
+    // operator.start().whileTrue(Commands.print("Pass Right")); // this might be switched with back
 
-    // Fine control
-    operator
-        .povUp()
-        .and(() -> fineControl)
-        .whileTrue(
-            this.robot.getShooterHood().moveAtVoltage(ShooterHoodConstants.FINE_CONTROL_VOLTAGE));
+    // // Fine control
+    // operator
+    //     .povUp()
+    //     .and(() -> fineControl)
+    //     .whileTrue(
+    //         this.robot.getShooterHood().moveAtVoltage(ShooterHoodConstants.FINE_CONTROL_VOLTAGE));
 
-    operator
-        .povDown()
-        .and(() -> fineControl)
-        .whileTrue(
-            this.robot
-                .getShooterHood()
-                .moveAtVoltage(ShooterHoodConstants.FINE_CONTROL_VOLTAGE.unaryMinus()));
+    // operator
+    //     .povDown()
+    //     .and(() -> fineControl)
+    //     .whileTrue(
+    //         this.robot
+    //             .getShooterHood()
+    //             .moveAtVoltage(ShooterHoodConstants.FINE_CONTROL_VOLTAGE.unaryMinus()));
 
-    operator
-        .povLeft()
-        .and(() -> fineControl)
-        .whileTrue(this.robot.getTurret().moveAtVoltage(TurretConstants.FINE_CONTROL_VOLTAGE));
+    // operator
+    //     .povLeft()
+    //     .and(() -> fineControl)
+    //     .whileTrue(this.robot.getTurret().moveAtVoltage(TurretConstants.FINE_CONTROL_VOLTAGE));
 
-    operator
-        .povLeft()
-        .and(() -> fineControl)
-        .whileTrue(
-            this.robot
-                .getTurret()
-                .moveAtVoltage(TurretConstants.FINE_CONTROL_VOLTAGE.unaryMinus()));
+    // operator
+    //     .povLeft()
+    //     .and(() -> fineControl)
+    //     .whileTrue(
+    //         this.robot
+    //             .getTurret()
+    //             .moveAtVoltage(TurretConstants.FINE_CONTROL_VOLTAGE.unaryMinus()));
 
     operator
         .axisGreaterThan(Axis.kLeftX.value, 0.5)
@@ -187,18 +187,18 @@ public class TeleopControls {
                 .getIntakeExtension()
                 .moveAtVoltage(IntakeExtensionConstants.FINE_CONTROL_VOLTAGE.unaryMinus()));
 
-    operator
-        .axisGreaterThan(Axis.kRightY.value, 0.5)
-        .and(() -> fineControl)
-        .whileTrue(this.robot.getClimb().moveAtVoltage(ClimbConstants.FINE_CONTROL_VOLTAGE));
+    // operator
+    //     .axisGreaterThan(Axis.kRightY.value, 0.5)
+    //     .and(() -> fineControl)
+    //     .whileTrue(this.robot.getClimb().moveAtVoltage(ClimbConstants.FINE_CONTROL_VOLTAGE));
 
-    operator
-        .axisLessThan(Axis.kRightY.value, -0.5)
-        .and(() -> fineControl)
-        .whileTrue(
-            this.robot.getClimb().moveAtVoltage(ClimbConstants.FINE_CONTROL_VOLTAGE.unaryMinus()));
+    // operator
+    //     .axisLessThan(Axis.kRightY.value, -0.5)
+    //     .and(() -> fineControl)
+    //     .whileTrue(
+    //         this.robot.getClimb().moveAtVoltage(ClimbConstants.FINE_CONTROL_VOLTAGE.unaryMinus()));
 
-    // Intake extension and retraction
+    // // Intake extension and retraction
     Trigger intakeRetract = operator.rightStick().or(driver.back());
     Trigger intakeExtend =
         intakeRetract.negate().and(RobotState::isTeleop).and(RobotState::isEnabled);
@@ -208,12 +208,12 @@ public class TeleopControls {
         robot.getIntakeExtension().extend().alongWith(robot.getIntakeRollers().intake()));
 
     // Climb retraction
-    Command autodescend = robot.getClimb().descend();
-    Trigger climbRetract =
-        new Trigger(() -> TeleopSwerveCommand.isClearToOverride(robot.getClimb(), autodescend))
-            .and(RobotState::isTeleop)
-            .and(RobotState::isEnabled);
+    // Command autodescend = robot.getClimb().descend();
+    // Trigger climbRetract =
+    //     new Trigger(() -> TeleopSwerveCommand.isClearToOverride(robot.getClimb(), autodescend))
+    //         .and(RobotState::isTeleop)
+    //         .and(RobotState::isEnabled);
 
-    climbRetract.onTrue(robot.getClimb().descend());
+    // climbRetract.onTrue(robot.getClimb().descend());
   }
 }
