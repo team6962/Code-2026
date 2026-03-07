ffe1dc5e53308b755ddc650ca8f5bb113b600f9a
The-REAL-Bol <munsoly@gmail.com>
2026-03-07 10:13:44 -0800

    spotlessapply

--- src/main/java/frc/robot/auto/AutoSegments.java
+++ src/main/java/frc/robot/auto/AutoSegments.java
@@ -1,101 +1,92 @@
 package frc.robot.auto;
 
 import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.Inches;
-import static edu.wpi.first.units.Units.Meters;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.RobotContainer;
 
 public class AutoSegments {
-    private RobotContainer robot;
+  private RobotContainer robot;
 
-    public AutoSegments(RobotContainer robot) {
-        this.robot = robot;
+  public AutoSegments(RobotContainer robot) {
+    this.robot = robot;
+  }
+
+  public Rotation2d orient() {
+    if (robot.getSwerveDrive().getHeading().gt(Degrees.of(90))
+        && robot.getSwerveDrive().getHeading().lt(Degrees.of(270))) {
+      return Rotation2d.k180deg;
+    } else {
+      return Rotation2d.kZero;
     }
+  }
 
-    public Rotation2d orient() {
-        if (robot.getSwerveDrive().getHeading().gt(Degrees.of(90)) && robot.getSwerveDrive().getHeading().lt(Degrees.of(270))) {
-            return Rotation2d.k180deg;
-        }
-        else {
-            return Rotation2d.kZero;
-        }
-    }
+  public Command driveToStart() {
+    return robot
+        .getSwerveDrive()
+        .driveTo(new Pose2d(FieldPositions.START_POSITION, Rotation2d.kZero));
+  }
 
-    public Command driveToStart() {
-        return robot.getSwerveDrive().driveTo(new Pose2d(FieldPositions.START_POSITION, Rotation2d.kZero));
-    }
+  public Command driveToMiddleAlliance() {
+    return robot.getSwerveDrive().driveTo(FieldPositions.ALLIANCE_ZONE_CENTER);
+  }
 
-    public Command driveToMiddleAlliance(){
-        return robot.getSwerveDrive().driveTo(FieldPositions.ALLIANCE_ZONE_CENTER);
-    }
+  public Command driveToMiddleNeutral() {
+    return robot.getSwerveDrive().driveTo(FieldPositions.NEUTRAL_ZONE_CENTER);
+  }
 
-    public Command driveToMiddleNeutral(){
-        return robot.getSwerveDrive().driveTo(FieldPositions.NEUTRAL_ZONE_CENTER);
-    }
+  /*
+   * drive to left trench from Neutral zone
+   */
+  public Command driveToLeftTrenchNeutral() {
+    return robot.getSwerveDrive().driveTo(FieldPositions.Trench.LEFT_NEUTRAL);
+  }
 
-    /*
-     * drive to left trench from Neutral zone
-     */
-    public Command driveToLeftTrenchNeutral(){
-        return robot.getSwerveDrive().driveTo(FieldPositions.Trench.LEFT_NEUTRAL);
-    }
+  /*
+   * drive to left trench from alliance zone
+   */
+  public Command driveToLeftTrenchAlliance() {
+    return robot
+        .getSwerveDrive()
+        .driveTo(new Pose2d(FieldPositions.Trench.LEFT_ALLIANCE, orient()));
+  }
 
-    /*
-     * drive to left trench from alliance zone
-     */
-    public Command driveToLeftTrenchAlliance(){
-        return robot.getSwerveDrive().driveTo(new Pose2d(FieldPositions.Trench.LEFT_ALLIANCE, orient()));
-    }
+  /*
+   * drive to right trench from Neutral zone
+   */
+  public Command driveToRightTrenchNeutral() {
+    return robot.getSwerveDrive().driveTo(FieldPositions.Trench.RIGHT_NEUTRAL);
+  }
 
-    /*
-     * drive to right trench from Neutral zone
-     */
-    public Command driveToRightTrenchNeutral(){
-        return robot.getSwerveDrive().driveTo(FieldPositions.Trench.RIGHT_NEUTRAL);
-    }
+  /*
+   * drive to right trench from alliance zone
+   */
+  public Command driveToRightTrenchAlliance() {
+    return robot
+        .getSwerveDrive()
+        .driveTo(new Pose2d(FieldPositions.Trench.RIGHT_ALLIANCE, orient()));
+  }
 
-    /*
-     * drive to right trench from alliance zone
-     */
-    public Command driveToRightTrenchAlliance(){
-        return robot.getSwerveDrive().driveTo(new Pose2d(FieldPositions.Trench.RIGHT_ALLIANCE, orient()));
-    }
+  public Command driveToHub() {
+    return robot.getSwerveDrive().driveTo(FieldPositions.HUB_FRONT);
+  }
 
-    public Command driveToHub(){
-        return robot.getSwerveDrive().driveTo(FieldPositions.HUB_FRONT);
-    }
+  public Command driveThroughRightTrenchIntoAlliance() {
+    return Commands.sequence(driveToRightTrenchNeutral(), driveToRightTrenchAlliance());
+  }
 
-    public Command driveThroughRightTrenchIntoAlliance() {
-        return Commands.sequence(
-            driveToRightTrenchNeutral(),
-            driveToRightTrenchAlliance()
-        );
-    }
+  public Command driveThroughRightTrenchIntoNeutral() {
+    return Commands.sequence(driveToRightTrenchAlliance(), driveToRightTrenchNeutral());
+  }
 
-    public Command driveThroughRightTrenchIntoNeutral() {
-        return Commands.sequence(
-            driveToRightTrenchAlliance(),
-            driveToRightTrenchNeutral()
-        );
-    }
+  public Command driveThroughLeftTrenchIntoNeutral() { // ??
+    return Commands.sequence(driveToLeftTrenchAlliance(), driveToLeftTrenchNeutral());
+  }
 
-    public Command driveThroughLeftTrenchIntoNeutral() { // ??
-        return Commands.sequence(
-            driveToLeftTrenchAlliance(),
-            driveToLeftTrenchNeutral()
-        );
-    }
-
-    public Command driveThroughLeftTrenchIntoAlliance() { //??
-        return Commands.sequence(
-            driveToLeftTrenchNeutral(),
-            driveToLeftTrenchAlliance()
-        );
-    }    
+  public Command driveThroughLeftTrenchIntoAlliance() { // ??
+    return Commands.sequence(driveToLeftTrenchNeutral(), driveToLeftTrenchAlliance());
+  }
 }
--- src/main/java/frc/robot/auto/FieldPositions.java
+++ src/main/java/frc/robot/auto/FieldPositions.java
@@ -3,38 +3,32 @@ package frc.robot.auto;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Meters;
 
-import edu.wpi.first.hal.FRCNetComm.tInstances;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 
 public class FieldPositions {
-    public static final Translation2d START_POSITION = new Translation2d(0, 0); // dummy start pose
-    public static final Translation2d ALLIANCE_ZONE_CENTER = new Translation2d(
-        Inches.of(91.055).in(Meters),
-        Inches.of(158.84).in(Meters));
+  public static final Translation2d START_POSITION = new Translation2d(0, 0); // dummy start pose
+  public static final Translation2d ALLIANCE_ZONE_CENTER =
+      new Translation2d(Inches.of(91.055).in(Meters), Inches.of(158.84).in(Meters));
 
-    public static class Trench { //values need to be tuned and need to check if in alliance or neutral zone
-        public static final Translation2d RIGHT_ALLIANCE = new Translation2d(  
-            Inches.of(115.61).in(Meters), //original = 165.61
-            Inches.of(24.92).in(Meters)
-            ); 
-        public static final Translation2d LEFT_ALLIANCE = new Translation2d(
-            Inches.of(115.61).in(Meters),
-            Inches.of(292.76).in(Meters));
-        //drive to trench on neutral side
-        public static final Translation2d RIGHT_NEUTRAL = new Translation2d(
-            Inches.of(248.61).in(Meters),
+  public static
+  class Trench { // values need to be tuned and need to check if in alliance or neutral zone
+    public static final Translation2d RIGHT_ALLIANCE =
+        new Translation2d(
+            Inches.of(115.61).in(Meters), // original = 165.61
             Inches.of(24.92).in(Meters));
-        public static final Translation2d LEFT_NEUTRAL = new Translation2d(
-            Inches.of(248.61).in(Meters), //original pos = 198.61
+    public static final Translation2d LEFT_ALLIANCE =
+        new Translation2d(Inches.of(115.61).in(Meters), Inches.of(292.76).in(Meters));
+    // drive to trench on neutral side
+    public static final Translation2d RIGHT_NEUTRAL =
+        new Translation2d(Inches.of(248.61).in(Meters), Inches.of(24.92).in(Meters));
+    public static final Translation2d LEFT_NEUTRAL =
+        new Translation2d(
+            Inches.of(248.61).in(Meters), // original pos = 198.61
             Inches.of(292.76).in(Meters));
-    }
-    
-    public static final Translation2d NEUTRAL_ZONE_CENTER = new Translation2d(
-        Inches.of(325.61).in(Meters),
-        Inches.of(158.84).in(Meters));
-    public static final Translation2d HUB_FRONT = new Translation2d(
-        Inches.of(158.84).in(Meters),
-        Inches.of(123.61).in(Meters));
+  }
+
+  public static final Translation2d NEUTRAL_ZONE_CENTER =
+      new Translation2d(Inches.of(325.61).in(Meters), Inches.of(158.84).in(Meters));
+  public static final Translation2d HUB_FRONT =
+      new Translation2d(Inches.of(158.84).in(Meters), Inches.of(123.61).in(Meters));
 }
