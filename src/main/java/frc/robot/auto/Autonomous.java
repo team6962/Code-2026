8ed0ef260920a9dcc21c4132d15f0a5966dceb5b
Brian <74162951+codeNinja-1@users.noreply.github.com>
2026-03-07 17:14:26 -0800

    Add NeutralIntake and ShootFuel

--- src/main/java/frc/robot/auto/Autonomous.java
+++ src/main/java/frc/robot/auto/Autonomous.java
@@ -1,22 +1,35 @@
 package frc.robot.auto;
 
+import static edu.wpi.first.units.Units.Meters;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.RobotContainer;
 
-/**
- * Contains autonomous command sequences that can be selected on the dashboard.
- */
+/** Contains autonomous command sequences that can be selected on the dashboard. */
 public class Autonomous {
   private RobotContainer robot;
   private TrenchDriving trench;
+  private NeutralIntake neutralIntake;
+  private ShootFuel shootFuel;
 
   public Autonomous(RobotContainer robot) {
     this.robot = robot;
     this.trench = new TrenchDriving(robot);
+    this.neutralIntake = new NeutralIntake(robot);
+    this.shootFuel = new ShootFuel(robot);
   }
 
   public Command neutralCycle() {
-    return Commands.sequence(trench.driveToNeutral(), trench.driveToAlliance());
+    return Commands.sequence(
+        trench.driveToNeutral(),
+        neutralIntake.intake(Meters.of(1)),
+        trench.driveToAlliance(),
+        shootFuel.shootAllFuel(),
+        trench.driveToNeutral(),
+        neutralIntake.intake(Meters.of(1), Meters.of(2.5)),
+        trench.driveToAlliance(),
+        shootFuel.shootAllFuel()
+    );
   }
 }
