490e3201d4bb1c090e0ab9e89ac3c90923e02e40
neelabhb-sudo <neelabhb@khanlabschool.org>
2026-03-10 19:01:07 -0700

    Updated Collect Fuel From Hub

--- src/main/java/frc/robot/auto/CollectFuelFromHub.java
+++ src/main/java/frc/robot/auto/CollectFuelFromHub.java
@@ -1,6 +1,7 @@
 package frc.robot.auto;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -17,17 +18,18 @@ public class CollectFuelFromHub {
     return Commands.sequence(
         robot
             .getSwerveDrive()
-            .driveTo(new Pose2d(FieldPositions.HUB_BACK, Rotation2d.fromDegrees(-150))),
-        Commands.parallel(robot.getIntakeRollers().intake(), robot.getHopper().load())
-            .withDeadline(robot.getSwerveDrive().driveTo(FieldPositions.Trench.RIGHT_NEUTRAL)));
+            .driveTo(new Pose2d(FieldPositions.HUB_BACK, Rotation2d.fromDegrees(-60))),
+            Commands.parallel(robot.getIntakeRollers().intake(), robot.getHopper().load())
+            .withDeadline(robot.getSwerveDrive().driveTo(new Pose2d(FieldPositions.Trench.RIGHT_NEUTRAL, Rotation2d.fromDegrees(150)))));
   }
 
   public Command intakeBehindHubLeft() {
     return Commands.sequence(
         robot
             .getSwerveDrive()
-            .driveTo(new Pose2d(FieldPositions.HUB_BACK, Rotation2d.fromDegrees(150))),
-        Commands.parallel(robot.getIntakeRollers().intake(), robot.getHopper().load())
-            .withDeadline(robot.getSwerveDrive().driveTo(FieldPositions.Trench.LEFT_NEUTRAL)));
+            .driveTo(new Pose2d(FieldPositions.HUB_BACK, Rotation2d.fromDegrees(60))),
+            Commands.parallel(robot.getIntakeRollers().intake(), robot.getHopper().load())
+            .withDeadline(robot.getSwerveDrive().driveTo(new Pose2d(FieldPositions.Trench.LEFT_NEUTRAL, Rotation2d.fromDegrees(150))))
+        );
   }
 }
