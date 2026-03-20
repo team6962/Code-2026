9e033f78295c1b3cc237388e15f34c359e3b74e4
rahul <rahularyabollini@gmail.com>
2026-03-19 22:03:14 -0700

    made auto defend zone orient robot rotation

--- src/main/java/frc/robot/auto/AutoZoneDefense.java
+++ src/main/java/frc/robot/auto/AutoZoneDefense.java
@@ -1,5 +1,7 @@
 package frc.robot.auto;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.RobotContainer;
@@ -15,6 +17,16 @@ public class AutoZoneDefense {
     OWN_ALLIANCE,
     NEUTRAL_ZONE,
     OPPOSING_ALLIANCE,
+  }
+
+  /* Orient the robot to the nearest 90-degree angle based on its current heading */
+  public Rotation2d orientRotation() {
+    double robotRotation =
+        robot.getSwerveDrive().getLocalization().getPosition2d().getRotation().getRadians();
+
+    double orientedRotation = Math.round(robotRotation / (Math.PI / 2.0)) * (Math.PI / 2.0);
+
+    return new Rotation2d(orientedRotation);
   }
 
   /* Determine the robot's current zone based on its X position on the field */
@@ -36,11 +48,18 @@ public class AutoZoneDefense {
       return Commands.sequence(
           robot
               .getSwerveDrive()
-              .driveTo(FieldPositions.OpposingSide.OPPOSING_ALLIANCE_RIGHT_TRENCH),
+              .driveTo(
+                  new Pose2d(
+                      FieldPositions.OpposingSide.OPPOSING_ALLIANCE_RIGHT_TRENCH,
+                      orientRotation())),
           robot.getSwerveDrive().lock());
     } else if (getZone() == Zone.NEUTRAL_ZONE) {
       return Commands.sequence(
-          robot.getSwerveDrive().driveTo(FieldPositions.OpposingSide.OPPOSING_NEUTRAL_RIGHT_TRENCH),
+          robot
+              .getSwerveDrive()
+              .driveTo(
+                  new Pose2d(
+                      FieldPositions.OpposingSide.OPPOSING_NEUTRAL_RIGHT_TRENCH, orientRotation())),
           robot.getSwerveDrive().lock());
     } else {
       return Commands.none();
@@ -51,11 +70,19 @@ public class AutoZoneDefense {
   public Command defendRightBump() {
     if (getZone() == Zone.OPPOSING_ALLIANCE) {
       return Commands.sequence(
-          robot.getSwerveDrive().driveTo(FieldPositions.OpposingSide.OPPOSING_ALLIANCE_RIGHT_BUMP),
+          robot
+              .getSwerveDrive()
+              .driveTo(
+                  new Pose2d(
+                      FieldPositions.OpposingSide.OPPOSING_ALLIANCE_RIGHT_BUMP, orientRotation())),
           robot.getSwerveDrive().lock());
     } else if (getZone() == Zone.NEUTRAL_ZONE) {
       return Commands.sequence(
-          robot.getSwerveDrive().driveTo(FieldPositions.OpposingSide.OPPOSING_NEUTRAL_RIGHT_BUMP),
+          robot
+              .getSwerveDrive()
+              .driveTo(
+                  new Pose2d(
+                      FieldPositions.OpposingSide.OPPOSING_NEUTRAL_RIGHT_BUMP, orientRotation())),
           robot.getSwerveDrive().lock());
     } else {
       return Commands.none();
@@ -66,11 +93,19 @@ public class AutoZoneDefense {
   public Command defendLeftBump() {
     if (getZone() == Zone.OPPOSING_ALLIANCE) {
       return Commands.sequence(
-          robot.getSwerveDrive().driveTo(FieldPositions.OpposingSide.OPPOSING_ALLIANCE_LEFT_BUMP),
+          robot
+              .getSwerveDrive()
+              .driveTo(
+                  new Pose2d(
+                      FieldPositions.OpposingSide.OPPOSING_ALLIANCE_LEFT_BUMP, orientRotation())),
           robot.getSwerveDrive().lock());
     } else if (getZone() == Zone.NEUTRAL_ZONE) {
       return Commands.sequence(
-          robot.getSwerveDrive().driveTo(FieldPositions.OpposingSide.OPPOSING_NEUTRAL_LEFT_BUMP),
+          robot
+              .getSwerveDrive()
+              .driveTo(
+                  new Pose2d(
+                      FieldPositions.OpposingSide.OPPOSING_NEUTRAL_LEFT_BUMP, orientRotation())),
           robot.getSwerveDrive().lock());
     } else {
       return Commands.none();
@@ -81,11 +116,19 @@ public class AutoZoneDefense {
   public Command defendLeftTrench() {
     if (getZone() == Zone.OPPOSING_ALLIANCE) {
       return Commands.sequence(
-          robot.getSwerveDrive().driveTo(FieldPositions.OpposingSide.OPPOSING_ALLIANCE_LEFT_TRENCH),
+          robot
+              .getSwerveDrive()
+              .driveTo(
+                  new Pose2d(
+                      FieldPositions.OpposingSide.OPPOSING_ALLIANCE_LEFT_TRENCH, orientRotation())),
           robot.getSwerveDrive().lock());
     } else if (getZone() == Zone.NEUTRAL_ZONE) {
       return Commands.sequence(
-          robot.getSwerveDrive().driveTo(FieldPositions.OpposingSide.OPPOSING_NEUTRAL_LEFT_TRENCH),
+          robot
+              .getSwerveDrive()
+              .driveTo(
+                  new Pose2d(
+                      FieldPositions.OpposingSide.OPPOSING_NEUTRAL_LEFT_TRENCH, orientRotation())),
           robot.getSwerveDrive().lock());
     } else {
       return Commands.none();
