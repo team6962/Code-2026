204c46a754ecdaf36d813a45844c93c19e1e164b
Brian <74162951+codeNinja-1@users.noreply.github.com>
2026-03-22 15:18:04 -0700

    Increase speed limit for passing

--- src/main/java/frc/robot/controls/TeleopControls.java
+++ src/main/java/frc/robot/controls/TeleopControls.java
@@ -328,13 +328,22 @@ public class TeleopControls {
 
     autoPassTrigger.whileTrue(autoPass);
 
-    operator
-        .rightTrigger()
-        .or(driver.back())
+    Trigger shootButtonsTrigger = operator.rightTrigger().or(driver.back());
+
+    shootButtonsTrigger
+        .and(inAllianceZone)
         .whileTrue(
             teleopSwerveCommand.limitVelocity(
                 MetersPerSecond.of(0.5), RotationsPerSecond.of(0.125))) // Temporary values
-        .and(autoShoot.isReadyToShoot().or(autoPass.isReadyToShoot()))
+        .and(autoShoot.isReadyToShoot())
+        .whileTrue(robot.getHopper().feed());
+
+    shootButtonsTrigger
+        .and(inAllianceZone.negate())
+        .whileTrue(
+            teleopSwerveCommand.limitVelocity(
+                MetersPerSecond.of(1.5), RotationsPerSecond.of(0.5))) // Temporary values
+        .and(autoPass.isReadyToShoot())
         .whileTrue(robot.getHopper().feed());
 
     // ShooterFunctions functions = robot.getHubFunctions();
