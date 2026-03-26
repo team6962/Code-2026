c0648e014c858841905e2a1b508a3d99035e587e
Brian <74162951+codeNinja-1@users.noreply.github.com>
2026-03-25 19:04:45 -0700

    Optimize FieldLogger

--- src/main/java/com/team6962/lib/swerve/util/FieldLogger.java
+++ src/main/java/com/team6962/lib/swerve/util/FieldLogger.java
@@ -8,6 +8,7 @@ import com.team6962.lib.swerve.localization.Odometry;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -125,7 +126,7 @@ public class FieldLogger implements SwerveComponent {
       isPublished = true;
     }
 
-    if (previousRobotPose != null) {
+    if (RobotBase.isSimulation() && previousRobotPose != null) {
       Pose2d updatedRobotPose = field.getRobotPose();
 
       if (!previousRobotPose.equals(updatedRobotPose)) {
@@ -156,8 +157,6 @@ public class FieldLogger implements SwerveComponent {
     synchronized (this) {
       logData = new LogData(robotPose, modulePoses);
     }
-
-    field.setRobotPose(robotPose);
   }
 
   /**
