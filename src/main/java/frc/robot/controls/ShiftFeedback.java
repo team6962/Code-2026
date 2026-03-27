b66bf6a5b2c1fcda346203e7fbfafd3cc7a22830
Brian <74162951+codeNinja-1@users.noreply.github.com>
2026-03-27 10:39:06 -0700

    Add transition period and improve rumble for shift feedback

--- src/main/java/frc/robot/controls/ShiftFeedback.java
+++ src/main/java/frc/robot/controls/ShiftFeedback.java
@@ -19,13 +19,16 @@ public class ShiftFeedback extends SubsystemBase {
   private double teleopStartTimeSeconds = -1;
 
   /** Whether to simulate the endgame phase when not in practice mode or an official match. */
-  private boolean includeEndgameInTesting = false;
+  private boolean includeEndgameInTesting = true;
 
   /**
    * Whether to simulate winning the autonomous phase when not in practice mode or an official
    * match.
    */
   private boolean winAutoInTesting = false;
+
+  /** Whether to enable feedback. */
+  private boolean feedbackEnabled = false;
 
   /**
    * Creates a new ShiftFeedback subsystem and schedules rumble commands on the provided
@@ -45,6 +48,8 @@ public class ShiftFeedback extends SubsystemBase {
 
     DogLog.tunable(
         "ShiftFeedback/WinAutoInTesting", winAutoInTesting, value -> winAutoInTesting = value);
+
+    DogLog.tunable("ShiftFeedback/Enabled", feedbackEnabled, value -> feedbackEnabled = value);
   }
 
   /**
@@ -53,8 +58,10 @@ public class ShiftFeedback extends SubsystemBase {
    * @return The rumble intensity, ranging from 0.0 to 1.0.
    */
   private double getRumbleIntensity() {
-    if (RobotState.isDisabled() || !RobotState.isTeleop())
-      return 0.0; // Don't rumble if we're disabled or not in teleop
+    if (RobotState.isDisabled()
+        || !RobotState.isTeleop()
+        || !feedbackEnabled
+        || teleopStartTimeSeconds < 0.0) return 0.0;
 
     double timeUntilSwitchSeconds = getTimeUntilSwitch();
 
@@ -70,21 +77,33 @@ public class ShiftFeedback extends SubsystemBase {
 
   @Override
   public void periodic() {
+    if (!feedbackEnabled && !isPlayingMatch()) {
+      DogLog.forceNt.log("ShiftFeedback/TeleopElapsedTime", NEARLY_ZERO);
+      DogLog.forceNt.log("ShiftFeedback/TeleopRemainingTime", NEARLY_ZERO);
+      DogLog.forceNt.log("ShiftFeedback/TimeUntilSwitch", NEARLY_ZERO);
+      DogLog.forceNt.log("ShiftFeedback/IsActive", false);
+      DogLog.forceNt.log("ShiftFeedback/HubStatus", "#000000");
+
+      return;
+    }
+
     if (RobotState.isEnabled() && teleopStartTimeSeconds < 0) {
       teleopStartTimeSeconds = Timer.getFPGATimestamp();
     } else if (RobotState.isDisabled() && teleopStartTimeSeconds >= 0) {
       teleopStartTimeSeconds = -1;
     }
 
+    DogLog.forceNt.log("ShiftFeedback/WonAuto", getWinAutoColor());
+
     if (teleopStartTimeSeconds >= 0.0) {
       double timeSinceTeleopStartSeconds = Timer.getFPGATimestamp() - teleopStartTimeSeconds;
 
       DogLog.forceNt.log(
           "ShiftFeedback/TeleopElapsedTime",
-          Math.min(130, Math.max(NEARLY_ZERO, timeSinceTeleopStartSeconds)));
+          Math.min(140, Math.max(NEARLY_ZERO, timeSinceTeleopStartSeconds)));
       DogLog.forceNt.log(
           "ShiftFeedback/TeleopRemainingTime",
-          Math.max(NEARLY_ZERO, 130.0 - timeSinceTeleopStartSeconds));
+          Math.max(NEARLY_ZERO, 140.0 - timeSinceTeleopStartSeconds));
       DogLog.forceNt.log(
           "ShiftFeedback/TimeUntilSwitch", Math.max(NEARLY_ZERO, getTimeUntilSwitch()));
       DogLog.forceNt.log("ShiftFeedback/IsActive", isHubActive());
@@ -116,8 +135,10 @@ public class ShiftFeedback extends SubsystemBase {
     } else if (RobotState.isTeleop()) {
       double timeSinceTeleopStart = Timer.getFPGATimestamp() - teleopStartTimeSeconds;
 
-      if (timeSinceTeleopStart <= 100.0) {
-        int shiftNumber = (int) (timeSinceTeleopStart / 25.0);
+      if (timeSinceTeleopStart <= 10.0) {
+        return true;
+      } else if (timeSinceTeleopStart <= 110.0) {
+        int shiftNumber = (int) ((timeSinceTeleopStart - 10.0) / 25.0);
 
         return shiftNumber % 2 == (didWinAuto() ? 1 : 0);
       } else {
@@ -136,17 +157,23 @@ public class ShiftFeedback extends SubsystemBase {
   private double getTimeUntilSwitch() {
     double timeSinceTeleopStartSeconds = Timer.getFPGATimestamp() - teleopStartTimeSeconds;
 
-    if ((isPlayingMatch() || includeEndgameInTesting) && timeSinceTeleopStartSeconds > 130.0) {
+    if ((isPlayingMatch() || includeEndgameInTesting)
+        && timeSinceTeleopStartSeconds < 10.0
+        && didWinAuto()) {
+      return 10.0 - timeSinceTeleopStartSeconds;
+    }
+
+    if ((isPlayingMatch() || includeEndgameInTesting) && timeSinceTeleopStartSeconds > 140.0) {
       return -1;
     }
 
     if ((isPlayingMatch() || includeEndgameInTesting)
-        && (timeSinceTeleopStartSeconds > 100.0
-            || (didWinAuto() && timeSinceTeleopStartSeconds > 75.0))) {
-      return 130.0 - timeSinceTeleopStartSeconds;
+        && (timeSinceTeleopStartSeconds > 110.0
+            || (didWinAuto() && timeSinceTeleopStartSeconds > 85.0))) {
+      return 140.0 - timeSinceTeleopStartSeconds;
     }
 
-    return 25.0 - (timeSinceTeleopStartSeconds % 25.0);
+    return 25.0 - ((timeSinceTeleopStartSeconds - 10.0) % 25.0);
   }
 
   /**
@@ -179,7 +206,20 @@ public class ShiftFeedback extends SubsystemBase {
           && currentAlliance.isPresent()
           && winningAlliance.get() == currentAlliance.get();
     } else {
-      return winAutoInTesting;
+      return RobotState.isEnabled() ? winAutoInTesting : false;
+    }
+  }
+
+  private String getWinAutoColor() {
+    if (isPlayingMatch()) {
+      Optional<Alliance> winningAlliance = getAutoWinningAlliance();
+      Optional<Alliance> currentAlliance = DriverStation.getAlliance();
+
+      return (winningAlliance.isPresent() && currentAlliance.isPresent())
+          ? (winningAlliance.get() == currentAlliance.get() ? "#00ff00" : "#ff0000")
+          : "#888888";
+    } else {
+      return RobotState.isEnabled() ? (winAutoInTesting ? "#00ff00" : "#ff0000") : "#888888";
     }
   }
 
@@ -206,9 +246,11 @@ public class ShiftFeedback extends SubsystemBase {
       return Math.floor(remainingTimeSeconds * 2.0) % 2 == 1 ? 1.0 : 0.0;
     } else if (remainingTimeSeconds >= 9.75 && remainingTimeSeconds <= 10.25) {
       return 0.75;
-    } else if (remainingTimeSeconds >= 14.75 && remainingTimeSeconds <= 15.25) {
+    } else if (remainingTimeSeconds >= 15 - (1.0 / 16.0)
+        && remainingTimeSeconds <= 15 + (1.0 / 16.0)) {
       return 0.5;
-    } else if (remainingTimeSeconds >= 19.75 && remainingTimeSeconds <= 20.25) {
+    } else if (remainingTimeSeconds >= 20 - (1.0 / 32.0)
+        && remainingTimeSeconds <= 20 + (1.0 / 32.0)) {
       return 0.25;
     }
 
